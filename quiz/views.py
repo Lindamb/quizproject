44f1e81390dafa8002f9155c229b4058540e0c23
Lindamb <linda@Lindas-MacBook-Pro.local>
2016-09-08 12:42:37 +0200

    Klistrat in Emils data för quiz i start och quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
-def start(request):
-	return render(request, "quiz/start.html")
+from django.shortcuts import render	
+quizzes = [
+	{
+		"quiz_number": 1,
+   		"name": "Klassiska böcker",
+	   	"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+   	   	"name": "Största 1slagen",
+	   	"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+   	    	"name": "Världens mest kända hackare",
+	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+]
+# Create your views here.
 
-def quiz(request):
+def start(request):
+	context = {
+			"quizzes": quizzes,
+	}
+	return render(request, "quiz/start.html", context)
+
+def quiz(request, quiz_number):
+	context = {
+		"quiz": quizzes[int(quiz_number) - 1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz/quiz.html", context)
 	return render(request, "quiz/quiz.html")
 
 def question(request):
 	return render(request, "quiz/question.html")	
 
 def results(request):
-	return render(request, "quiz/results.html")		
-
-# Create your views here.
+	return render(request, "quiz/results.html")	
