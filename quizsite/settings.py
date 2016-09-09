5bf00a5758dd9602bc928edb789d5f6bf9ce2f7e
Lindamb <linda@Lindas-MacBook-Pro.local>
2016-09-09 15:36:40 +0200

    ändrat

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -79,9 +79,7 @@ DATABASES = {
     default='sqlite:////{0}'.format(os.path.join(BASE_DIR,
 'db.sqlite3'))
     )
-
-    }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
