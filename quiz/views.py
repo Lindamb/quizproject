105fb9bfce9ffb40aa8332290c3336c328c66c85
Lindamb <linda@Lindas-MacBook-Pro.local>
2016-09-09 11:51:02 +0200

    Ändrat på startsidan
    Nu syns inte lägre Emils data.

--- quiz/views.py
+++ quiz/views.py
@@ -1,25 +1,9 @@
+from quiz.models import Quiz 
 from django.shortcuts import render	
-quizzes = [
-	{
-		"quiz_number": 1,
-   		"name": "Klassiska böcker",
-	   	"description": "Hur bra kan du dina klassiker?"
-	},
-	{
-		"quiz_number": 2,
-   	   	"name": "Största 1slagen",
-	   	"description": "Kan du dina lag?"
-	},
-	{
-		"quiz_number": 3,
-   	    "name": "Världens mest kända hackare",
-	    "description": "Hackerhistoria är viktigt, kan du den?"	},
-]
-# Create your views here.
 
 def start(request):
 	context = {
-			"quizzes": quizzes,
+			"quizzes": Quiz.objects.all();
 	}
 	return render(request, "quiz/start.html", context)
 
